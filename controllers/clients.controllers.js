a3dab83ed796878235eb1c95320579ffd40b7a6d
Nouman864 <noumanafzaljbd@gmail.com>
2020-05-11 17:48:45 +0500

    set up repo

--- controllers/clients.controllers.js
+++ controllers/clients.controllers.js
@@ -37,7 +37,7 @@ clientsController.registerUser = async (req, res) => {
         to : `${req.body.email}`, // receiver email2
         subject: "Account Verification",
         html: `<h1>Hello Friend Please Click on this link<h1><br>Token
-    <br><a href="https://mybookyap.herokuapp.com//clientverify?token=${token}">CLICK ME TO ACTIVATE YOUR ACCOUNT</a>`
+    <br><a href="https://mybookyap.herokuapp.com/clientverify?token=${token}">CLICK ME TO ACTIVATE YOUR ACCOUNT</a>`
       };
                                                             
       transporter.sendMail(mailOptions, function (err, info) {
