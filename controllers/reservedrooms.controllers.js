5d648b53544a45bcadd0f6289846048beb2b0b1d
Nouman864 <noumanafzaljbd@gmail.com>
2020-05-30 12:50:58 +0500

    set up repo

--- controllers/reservedrooms.controllers.js
+++ controllers/reservedrooms.controllers.js
@@ -9,9 +9,12 @@ roomController.bookroom = async (req, res) => {
       {
         console.log("knjlk");
         const clientid = body.client
-         const rmid = await  Reservedroom.findone({ "body._id": clientid})
-        console.log(rmid);
-      }
+        const rmid = await  Reservedroom.findOne(query, function(err, u)
+         {
+          body._id.equals(clientid)
+        })
+          console.log(rmid);
+    }
       else
       {
 
