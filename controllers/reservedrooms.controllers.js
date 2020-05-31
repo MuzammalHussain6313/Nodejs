f51910f80c3ff19cdfbbe5c0bbc9ea13e95c2698
Nouman864 <noumanafzaljbd@gmail.com>
2020-05-31 14:12:13 +0500

    set up repo

--- controllers/reservedrooms.controllers.js
+++ controllers/reservedrooms.controllers.js
@@ -1,7 +1,7 @@
 const roomController = {};
 const Reservedroom = require('../models/reservedrooms.model');
 const jsonwebtoken =  require('jsonwebtoken');
-roomController.bookroom = async (req, res) => {
+roomController.bookroom =  (req, res) => {
     try {
       let result;
       const body = req.body;
@@ -13,11 +13,7 @@ roomController.bookroom = async (req, res) => {
       {
         
          let Rmid = body.Room[i].Rooms[i].roomno.Roomid;
-         console.log(Rmid);
-        // const dte = await  Reservedroom.find({ "body.checkin":{ $lt: date }})
-
-        const rmid = await  Reservedroom.find({ "Room.Rooms.roomno.Roomid": Rmid})
-           console.log(rmid);
+        const rmid =   Reservedroom.find({ "Room.Rooms.roomno.Roomid": Rmid})
         if(rmid.length)
           {
              console.log(" roomId match");
@@ -36,7 +32,7 @@ roomController.bookroom = async (req, res) => {
     {
        const body = req.body;
           const reservedroom = new Reservedroom (body);
-         result = await reservedroom.save();
+         result =  reservedroom.save();
         
     }
     if(this.match === true)
@@ -62,23 +58,15 @@ roomController.bookroom = async (req, res) => {
     try {
       
       const body = req.body;
-        // let Rmid = body.roomqr;
-        // console.log(Rmid);
-          //  let Rmid = 111;
-          //  console.log(Rmid);
-           for (var i = 0; i < 1; i++)
-      {
         let Rmid = body.roomqr;
-        let ff = body.Room[i].Rooms[i].roomno.Roomid;
-        console.log(Rmid);
           const r = await  Reservedroom.find({ "Room.Rooms.roomno.Roomid": Rmid})
              console.log(r);
           if(r.length)
             {
-               console.log(" roomId match");
+               console.log(" scan verified");
             }
           }
-        }
+        
      catch (error) {
       console.log('error', error);
       console.log("czd");
