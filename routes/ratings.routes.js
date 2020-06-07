18a2d4b8d6d1083456c9d7a5542afc80895b6630
Nouman864 <noumanafzaljbd@gmail.com>
2020-06-07 19:13:30 +0500

    set up repo

--- routes/ratings.routes.js
+++ routes/ratings.routes.js
@@ -5,5 +5,5 @@ const ratingController = require('../controllers/ratings.controllers');
 const checkAuth = require('../middleware/check-auth');
 
 router.post("/add",checkAuth,ratingController.addreview);
-router.get("/:owner", checkAuth,ratingController.getrate);
+router.get("/", checkAuth,ratingController.getrate);
 module.exports = router;
