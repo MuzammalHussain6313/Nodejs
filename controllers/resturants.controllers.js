1a0d473e5bc366ab310b6b78e1d591f4b2945fdb
Nouman864 <noumanafzaljbd@gmail.com>
2020-06-11 07:45:00 +0500

    set up repo

--- controllers/resturants.controllers.js
+++ controllers/resturants.controllers.js
@@ -19,4 +19,28 @@ resturantsController.addresturant = async (req, res) => {
     }
   };
 
+
+  resturantsController.getAll = async (req, res) => {
+    let resturant;
+    try {
+      let merged = {};
+      const start = 0;
+      const length = 100;
+      resturant = await Resturants.paginate(
+        merged,
+        {
+          offset: parseInt(start),
+          limit: parseInt(length)
+        }
+      );
+      res.status(200).send({
+        code: 200,
+        message: 'Successful',
+        data: resturant
+      });
+    } catch (error) {
+      console.log('error', error);
+      return res.status(500).send(error);
+    }
+  };
   module.exports = resturantsController;
--- routes/resturants.routes.js
+++ routes/resturants.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const resturantController = require('../controllers/resturants.controllers');
 const checkAuth = require('../middleware/check-auth');
 
-// router.get("/", checkAuth,flatController.getAll);
+router.get("/", checkAuth,resturantControlle.getAll);
 router.post("/add",checkAuth,resturantController.addresturant);
 // router.get("/:owner", checkAuth,flatController.getFlat)
 // router.put("/:id", checkAuth,flatController.updateBook);
