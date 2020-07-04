201de6483de9a249685df8fa556050e10534fa8e
Nouman864 <noumanafzaljbd@gmail.com>
2020-07-04 15:18:16 +0500

    set up repo

--- routes/bookedtables.routes.js
+++ routes/bookedtables.routes.js
@@ -5,7 +5,7 @@ const bookedtableController = require('../controllers/bookedtables.controllers')
 const checkAuth = require('../middleware/check-auth');
 
 // router.get("/", checkAuth,hotelController.getAll);
-router.post("/add",checkAuth, bookedtableController.bookroom);
+router.post("/add",checkAuth, bookedtableController.booktable);
 // router.post("/scan",checkAuth,reservedroomController.scan);
 //  router.get("/:owner", checkAuth,hotelController.getHotel)
 //  router.put("/:id", checkAuth,hotelController.updateHotel);
